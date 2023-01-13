873bfdf626d295cd264b3c23550064634df7f80a
Vaclav Bubnik <vbubnik@email.cz>
2023-01-13 04:36:12 +0100

    Update.

--- classSegmentation.js
+++
@@ -1,4 +0,0 @@
-var classSegmentation =
-[
-    [ "Segmentation", "classSegmentation.html#a966891da37798baabb99bf0144d47675", null ]
-];
--- structVoxels_1_1Voxel.js
+++
@@ -1,8 +0,0 @@
-var structVoxels_1_1Voxel =
-[
-    [ "i", "structVoxels_1_1Voxel.html#a7824e7bb6055dc585b17c80678dc3555", null ],
-    [ "state", "structVoxels_1_1Voxel.html#a16bb17a7cb9851c2f4f1558d2f245591", null ],
-    [ "x", "structVoxels_1_1Voxel.html#a38141eab6775cf6dbe5eab016ae6aec3", null ],
-    [ "y", "structVoxels_1_1Voxel.html#a10b94fbf36d85bbc6ff8f57507f46f1c", null ],
-    [ "z", "structVoxels_1_1Voxel.html#a35931196fa2790025f4f9475a07dba63", null ]
-];
